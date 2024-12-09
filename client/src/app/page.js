baa718c43b4ad04aa62bb24fdd7c7327e9ee1b63
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2024-12-09 13:01:15 +0530

    update with name

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -129,8 +129,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p className="mb-2">Arcade Games !!!</p>
-
+        <p className="mb-2">Winfinity GAMES GAME Games !!!</p>
         <button
           className="px-4 py-2 bg-blue-500 text-white rounded-md"
           onClick={connectWallet}

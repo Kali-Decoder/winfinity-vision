327259c1aa17f57442d1d73bc51b49c7ab08e461
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2024-12-09 16:44:34 +0530

    Rainbow Kit

--- next-app-router/app/evm/page.tsx
+++
@@ -1,9 +0,0 @@
-import ReadAccount from "@/components/ReadAccount";
-export default function evm() {
-    
-    return (
-        <div>
-            <ReadAccount></ReadAccount>
-        </div>
-    )
-}

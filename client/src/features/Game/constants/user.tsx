993baeeeb0d807f1ed550ca83ef3028c84ff5d07
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2024-12-10 12:55:21 +0530

    update images

--- client/src/features/Game/constants/user.tsx
+++ client/src/features/Game/constants/user.tsx
@@ -9,30 +9,34 @@ export const games = [
   {
     name: 'Snake Game',
     gifSrc: 'https://assets.8thlight.com/images/insights/snake-tutorial-13.gif',
-    amountToDeposit: 100,
+    amountToDeposit: 10,
+    description:"A Classic Snake Game. Eat the food and grow your snake. Avoid hitting the walls or yourself.",
     points: 3200,
     redirectUrl:"/snake-game"
   },
   {
     name: 'Flappy Bird',
     gifSrc: 'https://media4.giphy.com/media/euuaA2cwLEUuI/giphy.gif',
-    amountToDeposit: 150,
+    amountToDeposit: 20,
+    description:"A Classic Flappy Bird Game. Tap to fly and avoid hitting the pipes.",
     points: 4000,
     redirectUrl:"/flappy-bird"
  
   },
   {
-    name: 'Flappy Bird',
-    gifSrc: 'https://media4.giphy.com/media/euuaA2cwLEUuI/giphy.gif',
-    amountToDeposit: 150,
+    name: 'Pin the Needle',
+    gifSrc: 'https://149493502.v2.pressablecdn.com/wp-content/uploads/2015/02/aa-level-24-screenshot.jpg',
+    amountToDeposit: 20,
+    description:"A Classic Pin the Needle Game. Tap to pin the needle and avoid hitting the other needles.",
     points: 4000,
     redirectUrl:"/flappy-bird"
  
   },
   {
-    name: 'Flappy Bird',
-    gifSrc: 'https://media4.giphy.com/media/euuaA2cwLEUuI/giphy.gif',
-    amountToDeposit: 150,
+    name: 'Pin Ball',
+    gifSrc: 'https://media0.giphy.com/media/l4HnZvdrGVGpv2Vm8/giphy.gif?cid=6c09b9524zgfltn9x0i2cesu59zlu6cx1395kvvi5f58qser&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g',
+    amountToDeposit: 30,
+    description:"A Classic Pin Ball Game. Tap to pin the ball and avoid hitting the other balls.",
     points: 4000,
     redirectUrl:"/flappy-bird"
  

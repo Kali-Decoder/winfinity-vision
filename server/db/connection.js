9532eefcd4e7003d187aa816e9a15ace8cbc53d9
Snehagupta1907 <snehagupta98930@gmail.com>
2024-12-13 01:09:33 +0530

    added game model

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,9 +1,12 @@
-const mongoose=require('mongoose');
-const DB=process.env.MONGO_DB_URL;
-mongoose.connect(DB,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-    
-}).then(()=>{
-    console.log("Database Connection Successfull !");
-}).catch((err)=>console.log("Not Connected !" + err))
+import mongoose from "mongoose";
+
+const DB = process.env.MONGO_DB_URL;
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Database Connection Successful!");
+  })
+  .catch((err) => {
+    console.log("Not Connected! " + err);
+  });

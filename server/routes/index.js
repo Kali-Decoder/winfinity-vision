207d91c3858e301ec20ee17e0db7729a91a10591
Snehagupta1907 <snehagupta98930@gmail.com>
2024-12-21 19:46:34 +0530

    update

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,23 @@
-import express from "express";
+import express from 'express';
+import {
+    registerUser,
+    depositAmount,
+    getLeaderboard,
+    withdrawAmount,
+    getAllGames,
+    addGame
+} from './controllers/userController.js';
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
-    res.send("Hello World");
-});
+// User Routes
+router.post('/users/register', registerUser);
+router.post('/users/deposit', depositAmount);
+router.get('/leaderboard', getLeaderboard);
+router.post('/users/withdraw', withdrawAmount);
+
+// Game Routes
+router.get('/games', getAllGames);
+router.post('/games', addGame);
 
 export default router;
